3a08c957e3b80df136d43a296c37632264404fb6
lchahua <62721646+edededd@users.noreply.github.com>
2022-05-22 16:04:44 -0400

    codes1

--- src/main/java/command/Luminosity.java
+++ src/main/java/command/Luminosity.java
@@ -2,16 +2,16 @@ package command;
 import receiver.Receiver;
 import java.util.logging.Logger;
 public class Luminosity implements Command{
-    private double luminosity;
+    private double lum;
     private Receiver receiver;
     Logger logger = Logger.getLogger(Luminosity.class.getName());
     public Luminosity(Receiver r, double l){
-        this.luminosity = l;
+        this.lum = l;
         this.receiver = r;
     }
     @Override
     public void execute(){
-         String s= "la nueva luminosidad es: "+luminosity;
+         String s= "la nueva luminosidad es: "+lum;
          logger.info(s);
     }
 }
